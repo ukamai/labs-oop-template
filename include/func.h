11ec1245e8af6930f7d4c20dc2c668bda4c2f02c
frolmuka <144661268+ukamai@users.noreply.github.com>
2024-09-21 14:11:05 +0300

    Update func.h

--- include/func.h
+++ include/func.h
@@ -1,11 +1,3 @@
 #include <string>
 
-int countVowels(const std::string& str) {
-    int count = 0;
-    for (char ch : str) {
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-            count++;
-        }
-    }
-    return count;
-}
+int countVowels();
